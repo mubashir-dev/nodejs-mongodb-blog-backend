06b9c6367de4dacbd5571cc44abab599b5e04bd8
mubashir-dev <mubashirmobi633@gmail.com>
2021-10-01 16:22:51 +0500

    Login has Added using JWT

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,6 +3,8 @@ const User = require("../models/user.model");
 const mongoose = require("mongoose");
 const httpError = require("http-errors");
 const passwordHash = require("../helpers/password.hash");
+const jwt = require("jsonwebtoken");
+const auth = require("../middlewares/jwt.auth");
 
 //register user
 exports.register = [
@@ -62,7 +64,60 @@ exports.register = [
     }];
 
 //login
-exports.login = async (req, res, next) => { };
+exports.login = [
+    body("email", "Email is required.").isEmail().trim(),
+    body("password", "Password is required.").isLength({ min: 6 }),
+    async (req, res, next) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                let _errors = [];
+                errors.array().forEach((element) => {
+                    _errors.push(element.msg);
+                });
+                next(new httpError(422, { message: _errors }));
+            }
+            else {
+                const foundUser = await User.findOne({ email: req.body.email })
+                if (!foundUser) {
+                    next(new httpError(404, 'User is not found in our system'));
+                }
+                //Compare password
+                if (passwordHash.compare(foundUser.password)) {
+                    //Check if user is active or suspended
+                    if (foundUser.status == '1') {
+                        let userData = {
+                            _id: foundUser._id,
+                            name: foundUser.name,
+                            email: foundUser.email,
+                            username: foundUser.username,
+                            role: foundUser.role,
+                            status: foundUser.status == 1 ? "Active" : "Suspend",
+                        };
+                        //Prepare JWT token for authentication
+                        const jwtPayload = userData;
+                        const jwtData = {
+                            expiresIn: process.env.JWT_TIMEOUT_DURATION,
+                        };
+                        const secret = process.env.JWT_SECRET;
+                        //Generated JWT token with Payload and secret.
+                        userData.token = jwt.sign(jwtPayload, secret, jwtData);
+                        res.status(200).send({ user: userData })
+                    }
+                    else {
+                        next(new httpError(400, 'Your Account is not activated'));
+                    }
+                }
+
+
+                res.status(200).send({ user: foundUser });
+            }
+        }
+        catch (error) {
+            next(new httpError(500, { message: error.message }));
+        }
+    }
+];
 
 //get all users,this is only for admin
 exports.findall = async (req, res, next) => { };
