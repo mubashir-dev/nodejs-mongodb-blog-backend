52915a6b0be57782f3ca345d640f2ba42ff6a9bb
mubashir-dev <mubashirmobi633@gmail.com>
2021-10-01 11:42:49 -0700

    Authentication has completed

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,9 +4,11 @@ const AuthController = require('../controllers/AuthController')
 
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
-router.get('/', AuthController.findall);
+router.get('/', AuthController.index);
 router.get('/:id', AuthController.find);
-router.patch('/deactivate/:id', AuthController.deactivate);
-router.patch('/activate/:id', AuthController.activate);
+router.put('/update/:id', AuthController.update);
+router.put('/changePassword/:id', AuthController.changePassword);
+router.put('/deactivate/:id', AuthController.deactivate);
+router.put('/activate/:id', AuthController.activate);
 
 module.exports = router
