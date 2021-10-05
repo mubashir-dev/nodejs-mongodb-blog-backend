0bace2b5e253cd0b07a227e1d30695d43d4888e8
mubashir-dev <mubashirmobi633@gmail.com>
2021-10-05 13:49:43 -0700

    Post module has completed

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -6,14 +6,16 @@ const {
 const Post = require("../models/post.model");
 const mongoose = require("mongoose");
 var ObjectId = require('mongoose').Types.ObjectId;
+const ObjectID = require('mongodb').ObjectID;
 const httpError = require("http-errors");
 const passwordHash = require("../helpers/password.hash");
 const jwt = require("jsonwebtoken");
 const auth = require("../middlewares/jwt.auth");
 const userData = require("../helpers/user");
 const { v4: uuidv4 } = require('uuid');
-
 const multer = require('multer');
+const User = require("../models/user.model");
+const PostCategory = require("../models/post.category.model");
 //multer configuration
 var storage = multer.diskStorage({
     destination: function (req, file, callback) {
@@ -27,63 +29,60 @@ var storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 exports.upload = upload
 
+exports.index = [
+    auth,
+    async (req, res, next) => {
+        try {
+            const authUser = userData.user(req.headers.authorization);
+            const result = await Post.find({
+                user: authUser._id
+            });
+            if (!result) {
+                next(new httpError(200, {
+                    message: 'Record Not Found'
+                }))
+            }
+            res.send({
+                data: result
+            });
+        } catch (error) {
+            next(new httpError(500, {
+                message: error.message
+            }));
+        }
+    }
+]
 
+exports.find = [auth, async (req, res, next) => {
+    try {
+        const authUser = userData.user(req.headers.authorization);
+        if (!ObjectId.isValid(req.params.id)) {
+            next(new httpError(200, {
+                message: 'Passed Post Id is invalid '
+            }))
 
-//upload for post image
-// var upload = multer({ storage: storage }).single('image');
+        } else {
+            const result = await Post.findOne({
+                _id: req.params.id,
+                user: authUser._id
+            });
+            if (!result) {
+                next(new httpError(200, {
+                    message: 'Record Not Found'
+                }))
+            }
+            res.send({
+                data: result
+            });
+        }
+    } catch (error) {
+        next(new httpError(500, {
+            message: error.message
+        }));
+    }
+}
+];
 
-// exports.index = [
-//     auth,
-//     async (req, res, next) => {
-//         try {
-//             const authUser = userData.user(req.headers.authorization);
-//             const result = await PostCategory.find({
-//                 user: authUser._id
-//             });
-//             if (!result) {
-//                 next(new httpError(200, {
-//                     message: 'Record Not Found'
-//                 }))
-//             }
-//             res.send({
-//                 data: result
-//             });
-//         } catch (error) {
-//             next(new httpError(500, {
-//                 message: error.message
-//             }));
-//         }
-//     }
-// ]
-
-// exports.find = async (req, res, next) => {
-//     try {
-//         const authUser = userData.user(req.headers.authorization);
-//         if (!ObjectId.isValid(req.params.id)) {
-//             next(new httpError(200, {
-//                 message: 'Passed Post Category Id is invalid '
-//             }))
-
-//         } else {
-//             const result = await PostCategory.findOne({
-//                 _id: req.params.id,
-//                 user: authUser._id
-//             });
-//             if (!result) {
-//                 next(new httpError(200, {
-//                     message: 'Record Not Found'
-//                 }))
-//             }
-//             res.send({
-//                 data: result
-//             });
-//         }
-//     } catch (error) {
-//         next(new httpError(500, {
-//             message: error.message
-//         }));
-//     }
-// };
 exports.create = [auth,
     body("title", "Post title must be specified.").isLength({
         min: 6
@@ -95,8 +94,19 @@ exports.create = [auth,
     body("tags", "Post must at least 1 tag.").isLength({
         min: 6
     }),
-    check('category_id', 'Post Category  Id must be specified')
-        .trim(),
+    body('category_id', 'Post Category  Id must be specified')
+        .trim().custom((value, {
+            req
+        }) => {
+
+            return PostCategory.findOne({
+                _id: ObjectID(req.body.category_id)
+            }).then(PostCategory => {
+                if (!PostCategory) {
+                    return Promise.reject("Post Category has not been found.");
+                }
+            });
+        }),
     async function (req, res, next) {
         try {
             const errors = validationResult(req);
@@ -118,7 +128,7 @@ exports.create = [auth,
             else {
                 //upload file
                 try {
-
+                    upload.single('image')
                 }
                 catch (err) {
                     console.log("Error has occured while uploading file");
@@ -141,165 +151,207 @@ exports.create = [auth,
         }
     },
 ]
+exports.update = [auth,
+    body("title", "Post title must be specified.").isLength({
+        min: 6
+    }).trim(),
 
-// exports.update = [auth,
-//     body("title", "Post Category title must be specified.").isLength({
-//         min: 6
-//     }).withMessage("Post Category title must be  at least 6 characters long").trim(),
-//     async function (req, res, next) {
+    body("body", "Post Body must be specified.").isLength({
+        min: 200
+    }).withMessage("Post body must be at least 200 characters long").trim(),
+    body("tags", "Post must at least 1 tag.").isLength({
+        min: 6
+    }),
+    body('category_id', 'Post Category  Id must be specified')
+        .trim().custom((value, {
+            req
+        }) => {
+            return PostCategory.findOne({
+                _id: ObjectID(req.body.category_id)
+            }).then(PostCategory => {
+                if (!PostCategory) {
+                    return Promise.reject("Post Category has not been found.");
+                }
+            });
+        }),
+    async function (req, res, next) {
+        const authUser = userData.user(req.headers.authorization);
+        if (!ObjectId.isValid(req.params.id)) {
+            next(new httpError(400,'Passed Post Id is invalid'))
+        } else {
+            const foundPost = await Post.findOne({
+                _id: req.params.id,
+                user: authUser._id
+            });
+            if (!foundPost) {
+                next(new httpError(404, 'Post not found against this ID'));
+            }
+            else {
+                try {
+                    const errors = validationResult(req);
+                    const authUser = userData.user(req.headers.authorization);
+                    if (!errors.isEmpty()) {
+                        let _errors = [];
+                        errors.array().forEach((element) => {
+                            _errors.push(element.msg);
+                        });
+                        next(new httpError(422, {
+                            message: _errors
+                        }));
+                    }
+                    else {
+                        //Checking if the file is present in request
+                        if(req.file)
+                        {
+                            try {
+                                upload.single('image')
+                            }
+                            catch (err) {
+                                console.log("Error has occured while uploading file");
+                            }
+                            const Updatedpost = foundPost({
+                                title: req.body.title,
+                                image: "/public/posts/" + req.file.filename,
+                                body: req.body.body,
+                                tags: req.body.tags,
+                                category: req.body.category_id,
+                                user: authUser._id
+                            });
+                        }
+                        else
+                        {
+                            const Updatedpost = foundPost({
+                                title: req.body.title,
+                                image: "/public/posts/" + req.file.filename,
+                                body: req.body.body,
+                                tags: req.body.tags,
+                                category: req.body.category_id,
+                                user: authUser._id
+                            });
+                        }
+                        
+                        let result = await Updatedpost.update();
+                        res.status(200).send({ post: result });
+                    }
+                } catch (error) {
+                    next(new httpError(500, {
+                        message: error.message
+                    }));
+                }
+            }
+        }
+    }
+]
+exports.delete = [auth,
+    async (req, res, next) => {
+        try {
+            if (!ObjectId.isValid(req.params.id)) {
+                next(new httpError(200, {
+                    message: 'Passed Post Id is invalid '
+                }))
+            } else {
+                const authUser = userData.user(req.headers.authorization);
+                const result = await Post.findOneAndDelete({
+                    _id: req.params.id,
+                    user: authUser._id
+                });
+                if (!result) {
+                    res.status(404).send({
+                        message: 'Post Category has not been deleted'
+                    });
+                }
+                else {
+                    res.send({
+                        message: 'Post Category has been deleted',
+                        data: result
+                    });
+                }
 
-//         try {
-//             if (!ObjectId.isValid(req.params.id)) {
-//                 next(new httpError(200, {
-//                     message: 'Passed Post Category Id is invalid '
-//                 }))
-//             } else {
-//                 const foundPostCategory = await PostCategory.findOne({
-//                     _id: req.params.id
-//                 })
-//                 if (!foundPostCategory) {
-//                     next(new httpError(404, 'Post Category not found against this ID'));
-//                 } else {
-//                     const errors = validationResult(req);
-//                     if (!errors.isEmpty()) {
-//                         let _errors = [];
-//                         errors.array().forEach((element) => {
-//                             _errors.push(element.msg);
-//                         });
-//                         next(new httpError(200, {
-//                             message: _errors
-//                         }));
-//                     } else {
-//                         const result = await PostCategory.findByIdAndUpdate(req.params.id, req.body);
-//                         res.send({
-//                             message: 'Post Category has been updated successfully',
-//                             data: result
-//                         });
-//                     }
+            }
+        } catch (error) {
+            next(new httpError(500, {
+                message: error.message
+            }))
+        }
+    }
+]
 
-//                 }
-//             }
+//Deactivate post 
+exports.deactivate = [auth,
+    async (req, res, next) => {
+        try {
+            const userData = userData.user(req.headers.authorization);
+            const errors = validationResult(req);
+            if (req.params.id == undefined) {
+                next(new httpError(422, {
+                    message: "Post must be specified"
+                }));
+            } else {
+                let post = await Post.findOne({
+                    _id: req.params.id,
+                    user:userData._id
+                });
+                if (user) {
+                    let result = await Post.findByIdAndUpdate({
+                        _id: req.params.id,
+                        user: userData._id
+                    }, {
+                        status: '0'
+                    });
+                    res.status(200).send({
+                        user: result,
+                        message: "The post has been deactivated"
+                    });
+                } else {
+                    res.status(404).send({
+                        message: "The Post has not been found"
+                    });
+                }
+            }
+        } catch (error) {
+            next(new httpError(500, {
+                message: error.message
+            }));
+        }
+    }
+];
+//activate post
+exports.activate = [auth,
+    async (req, res, next) => {
+        try {
+            const userData = userData.user(req.headers.authorization);
+            const errors = validationResult(req);
+            if (req.params.id == undefined) {
+                next(new httpError(422, {
+                    message: "The Post id must be specified"
+                }));
+            } else {
+                let user = await Post.findOne({
+                    _id: req.params.id,
+                    user: userData._id
+                });
+                if (user) {
+                    let result = await Post.findByIdAndUpdate({
+                        _id: req.params.id,
+                        user: userData._id
+                    }, {
+                        status: '1'
+                    });
+                    res.status(200).send({
+                        user: result,
+                        message: "The Post has been activated"
+                    });
 
-//         } catch (error) {
-//             next(new httpError(500, {
-//                 message: error.message
-//             }));
-//         }
-//     },
-// ]
-
-// exports.delete = [auth,
-//     async (req, res, next) => {
-//         try {
-//             if (!ObjectId.isValid(req.params.id)) {
-//                 next(new httpError(200, {
-//                     message: 'Passed Post Category Id is invalid '
-//                 }))
-//             } else {
-//                 const result = await PostCategory.findByIdAndDelete(req.params.id);
-//                 res.send({
-//                     message: 'Post Category has been deleted',
-//                     data: result
-//                 });
-//             }
-//         } catch (error) {
-//             next(new httpError(500, {
-//                 message: error.message
-//             }))
-//         }
-//     }
-// ]
-
-// //Deactivate post category
-// exports.deactivate = [auth,
-//     async (req, res, next) => {
-//         try {
-//             const errors = validationResult(req);
-//             if (req.params.id == undefined) {
-//                 next(new httpError(422, {
-//                     message: "Post Category must be specified"
-//                 }));
-//             } else {
-//                 let user = await User.findOne({
-//                     _id: req.params.id
-//                 });
-//                 if (user) {
-//                     const userData = userData.user(req.headers.authorization);
-//                     let result = await PostCategory.findByIdAndUpdate({
-//                         _id: req.params.id,
-//                         user: userData._id
-//                     }, {
-//                         status: '0'
-//                     });
-//                     res.status(200).send({
-//                         user: result,
-//                         message: "The post category has been deactivated"
-//                     });
-//                     // if (userData.role == "admin") {
-//                     //     let result = await User.findByIdAndUpdate({
-//                     //         status: req.params.id
-//                     //     }, {
-//                     //         status: '0'
-//                     //     });
-//                     //     res.status(200).send({
-//                     //         user: result,
-//                     //         message: "The user has been deactivated"
-//                     //     });
-//                     // } else {
-//                     //     res.status(200).send({
-//                     //         message: "Permission denied"
-//                     //     });
-//                     // }
-
-//                 } else {
-//                     res.status(404).send({
-//                         message: "The Post Category has not been found"
-//                     });
-//                 }
-//             }
-//         } catch (error) {
-//             next(new httpError(500, {
-//                 message: error.message
-//             }));
-//         }
-//     }
-// ];
-// //activate post category
-// exports.activate = [auth,
-//     async (req, res, next) => {
-//         try {
-//             const errors = validationResult(req);
-//             if (req.params.id == undefined) {
-//                 next(new httpError(422, {
-//                     message: "The Post Category id must be specified"
-//                 }));
-//             } else {
-//                 let user = await User.findOne({
-//                     _id: req.params.id
-//                 });
-//                 if (user) {
-//                     const userData = userData.user(req.headers.authorization);
-//                     let result = await PostCategory.findByIdAndUpdate({
-//                         _id: req.params.id,
-//                         user: userData._id
-//                     }, {
-//                         status: '1'
-//                     });
-//                     res.status(200).send({
-//                         user: result,
-//                         message: "The Post Category has been activated"
-//                     });
-
-//                 } else {
-//                     res.status(404).send({
-//                         message: "The Post Category has not been found"
-//                     });
-//                 }
-//             }
-//         } catch (error) {
-//             next(new httpError(500, {
-//                 message: error.message
-//             }));
-//         }
-//     }
-// ];
+                } else {
+                    res.status(404).send({
+                        message: "The Post has not been found"
+                    });
+                }
+            }
+        } catch (error) {
+            next(new httpError(500, {
+                message: error.message
+            }));
+        }
+    }
+];
--- routes/post.route.js
+++ routes/post.route.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const PostController = require('../controllers/PostController')
 
-router.post('/add', PostController.upload.single("myFile"), PostController.create);
-// router.get('/', PostCategoryController.index);
-// router.get('/:id', PostCategoryController.find);
-// router.put('/edit/:id', PostCategoryController.update);
-// router.delete('/:id', PostCategoryController.delete);
-// router.put('/deactivate/:id', PostCategoryController.deactivate);
-// router.put('/activate/:id', PostCategoryController.activate);
+router.post('/add', PostController.upload.single("image"), PostController.create);
+router.get('/', PostController.index);
+router.get('/:id', PostController.find);
+router.put('/edit/:id',PostController.upload.single("image"),PostController.update);
+router.delete('/:id', PostController.delete);
+router.put('/deactivate/:id', PostCategoryController.deactivate);
+router.put('/activate/:id', PostCategoryController.activate);
 
 module.exports = router
