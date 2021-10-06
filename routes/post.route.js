e93ef389314220928fccab29e3d97e0281ae548d
mubashir-dev <mubashirmobi633@gmail.com>
2021-10-06 13:26:51 -0700

    Added Post Collection for public access

--- routes/post.route.js
+++ routes/post.route.js
@@ -4,10 +4,11 @@ const PostController = require('../controllers/PostController')
 
 router.post('/add', PostController.upload.single("image"), PostController.create);
 router.get('/', PostController.index);
+router.get('/all-public-posts', PostController.all);
 router.get('/:id', PostController.find);
 router.put('/edit/:id',PostController.upload.single("image"),PostController.update);
 router.delete('/:id', PostController.delete);
-router.put('/deactivate/:id', PostCategoryController.deactivate);
-router.put('/activate/:id', PostCategoryController.activate);
+router.put('/deactivate/:id', PostController.deactivate);
+router.put('/activate/:id', PostController.activate);
 
 module.exports = router
